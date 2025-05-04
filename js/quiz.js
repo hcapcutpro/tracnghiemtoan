c9469276bdcda9ff8ed3a02f4535517fc6e6e4fe
Mow <hoang-vo@ecombest.com>
2025-05-04 18:35:47 +0700

    update link

--- js/quiz.js
+++ js/quiz.js
@@ -22,7 +22,10 @@ const subjectFileMap = {
 function selectSubject(subject) {
   window.subject = subject;
   if (subject && subjectFileMap[subject]) {
-    fetch("questions/" + subjectFileMap[subject])
+    fetch(
+      "https://raw.githubusercontent.com/hcapcutpro/tracnghiemtoan/refs/heads/main/questions/" +
+        subjectFileMap[subject]
+    )
       .then((res) => res.json())
       .then((data) => {
         currentQuiz = data;
